17e640fe82b33b700e508320954591db3a7a7c11
“Abbie <abbie.coghlan@gmail.com>
2021-02-02 11:34:29 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function() {
+  addingEventListener()
+}); 
+
+
+function addingEventListener(){
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let userInput = event.target[0].value;
+    let toDoUl = document.querySelector("#tasks");
+    let newLi = document.createElement("li");
+    newLi.innerText = userInput;
+    toDoUl.append(newLi);
+    event.target[0].value = "";
+  });  
+}
+
